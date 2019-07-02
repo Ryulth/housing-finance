431286444fae47bcdd97813e12b2eeed7c53988d
Ryulth <adboy94@khu.ac.kr>
2019-07-02 11:41:37 +0900

    feat: 연도와 ID로 매핑하면서 바로 계산하도록 변경

--- src/main/java/kakaopay/housingfinance/service/FinanceService.java
+++ src/main/java/kakaopay/housingfinance/service/FinanceService.java
@@ -1,6 +1,5 @@
 package kakaopay.housingfinance.service;
 
-import kakaopay.housingfinance.entity.Bank;
 import kakaopay.housingfinance.entity.BankFinance;
 import kakaopay.housingfinance.pojo.FinanceStatusByYear;
 import kakaopay.housingfinance.repository.BankFinanceRepository;
@@ -22,30 +21,64 @@ public class FinanceService {
         this.bankFinanceRepository = bankFinanceRepository;
     }
 
-    public Map<String, Object> getFinanceStatus() {
-        List<Bank> banks = bankRepository.findAll();
-        List<FinanceStatusByYear> financeStatusByYears = new ArrayList<>();
+    /*
+        public Map<String, Object> getFinanceStatus() {
+            List<Bank> banks = bankRepository.findAll();
+            List<FinanceStatusByYear> financeStatusByYears = new ArrayList<>();
 
-        for (Bank bank : banks) {
-            List<BankFinance> bankFinances = bankFinanceRepository.findAllByBankId(bank.getId());
-            Map<Integer, List<BankFinance>> bankFinanceMap = filterByYear(bankFinances);
-            System.out.println(bank.getName());
-            
+            for (Bank bank : banks) {
+                List<BankFinance> bankFinances = bankFinanceRepository.findAllByBankId(bank.getId());
+                Map<Integer, List<BankFinance>> bankFinanceMap = filterByYear(bankFinances);
+                System.out.println(bank.getName());
+
+            }
+
+            Map<String, Object> resultMap = new HashMap<>();
+            resultMap.put("name", "주택금융 공급현황");
+            return resultMap;
         }
 
+        private Map<Integer, List<BankFinance>> filterByYear(List<BankFinance> bankFinances) {
+            Map<Integer, List<BankFinance>> bankFinanceMap = new HashMap<>();
+            for (BankFinance bankFinance : bankFinances) {
+                bankFinanceMap.putIfAbsent(bankFinance.getYear(), new ArrayList<>());
+                bankFinanceMap.get(bankFinance.getYear())
+                        .add(bankFinance);
+            }
+            return bankFinanceMap;
+        }
+    */
+    public Map<String, Object> getFinanceStatus() {
+        List<BankFinance> bankFinances = bankFinanceRepository.findAll();
+        List<FinanceStatusByYear> financeStatusByYears = new ArrayList<>();
+        Map<String, Integer> bankFinanceMap = calcByYearAndBankId(bankFinances); // key == bankId/year
+        bankFinanceMap.forEach((key, value) -> {
+            String[] keys = key.split("/");
+            Long bankId = Long.valueOf(keys[0]);
+            Integer year = Integer.valueOf(keys[1]);
+            System.out.println(value);
+
+
+        });
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("name", "주택금융 공급현황");
         return resultMap;
     }
 
-    private Map<Integer, List<BankFinance>> filterByYear(List<BankFinance> bankFinances) {
-        Map<Integer, List<BankFinance>> bankFinanceMap = new HashMap<>();
+    private Map<String,Integer> calcByYearAndBankId(List<BankFinance> bankFinances) {
+        Map<String, Integer> bankFinanceMap = new HashMap<>();
+        StringBuilder stringBuilder = new StringBuilder();
         for (BankFinance bankFinance : bankFinances) {
-            bankFinanceMap.putIfAbsent(bankFinance.getYear(), new ArrayList<>());
-            bankFinanceMap.get(bankFinance.getYear())
-                    .add(bankFinance);
+            String key = stringBuilder.append(bankFinance.getBankId())
+                    .append("/")
+                    .append(bankFinance.getYear())
+                    .toString();
+            stringBuilder.setLength(0);
+            bankFinanceMap.putIfAbsent(key, 0);
+//            bankFinanceMap.get(key)
+            bankFinanceMap.compute(key, (s, integer) -> integer + bankFinance.getAmount());
+
         }
         return bankFinanceMap;
     }
-
 }
