e9bbc2e6aa09eaa1fb1020e794994f61f0264354
Ryulth <adboy94@khu.ac.kr>
2019-07-04 00:40:22 +0900

    feat: signIn 로직 구현 & exception 메시지 추가

--- src/main/java/kakaopay/account/controller/AccountController.java
+++ src/main/java/kakaopay/account/controller/AccountController.java
@@ -7,11 +7,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.Collections;
 
 @RestController
 public class AccountController {
@@ -30,12 +33,28 @@ public class AccountController {
             return new ResponseEntity<>(accountService.signUp(userDto), httpHeaders, HttpStatus.OK);
         }
         catch (EntityExistsException e){
-            return new ResponseEntity<>(httpHeaders, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(Collections.singletonMap("error",e.getMessage()),httpHeaders, HttpStatus.BAD_REQUEST);
         }
         catch (Exception e){
             logger.error(e.toString());
             return new ResponseEntity<>(httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
+
+    @PostMapping("/signin")
+    public ResponseEntity signIn(
+            @RequestBody UserDto userDto) {
+        try{
+            return new ResponseEntity<>(accountService.signIn(userDto), httpHeaders, HttpStatus.OK);
+        }
+        catch (EntityNotFoundException|BadCredentialsException e){
+            return new ResponseEntity<>(Collections.singletonMap("error",e.getMessage()),httpHeaders, HttpStatus.BAD_REQUEST);
+        }
+        catch (Exception e){
+            logger.error(e.toString());
+            return new ResponseEntity<>(httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
--- src/main/java/kakaopay/account/service/AccountService.java
+++ src/main/java/kakaopay/account/service/AccountService.java
@@ -3,8 +3,10 @@ package kakaopay.account.service;
 import kakaopay.account.dto.UserDto;
 import kakaopay.account.entity.User;
 import kakaopay.account.repository.UserRepository;
+import org.springframework.security.BadCredentialsException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityExistsException;
@@ -15,22 +17,35 @@ import java.util.Map;
 public class AccountService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
     public AccountService(UserRepository userRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     public Map signUp(UserDto userDto) {
-        if(userRepository.existsByEmail(userDto.getEmail())){
+        if (userRepository.existsByEmail(userDto.getEmail())) {
             throw new EntityExistsException("Email is duplicated");
         }
 
         User user = User.builder()
                 .email(userDto.getEmail())
-                .password( passwordEncoder.encode(userDto.getPassword()))
+                .password(passwordEncoder.encode(userDto.getPassword()))
                 .build();
 
         userRepository.save(user);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("jwt", "token");
+        return resultMap;
+    }
+
+    public Map signIn(UserDto userDto) {
+        User user = userRepository.findByEmail(userDto.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+        if (!passwordEncoder.matches(userDto.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Incorrect password");
+        }
 
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("jwt", "token");
