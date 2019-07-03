dc0ad203e7f9f90203e69ae2bcc3201c1fa12fd3
Ryulth <adboy94@khu.ac.kr>
2019-07-03 11:42:43 +0900

    refactor: FinanceService 멤버  메소드 분리

--- src/main/java/kakaopay/housingfinance/service/FinanceService.java
+++ src/main/java/kakaopay/housingfinance/service/FinanceService.java
@@ -15,45 +15,30 @@ import java.util.stream.Collectors;
 public class FinanceService {
     private final BankRepository bankRepository;
     private final BankFinanceRepository bankFinanceRepository;
+    private final PreProcessService preProcessService;
 
-    public FinanceService(BankRepository bankRepository, BankFinanceRepository bankFinanceRepository) {
+    public FinanceService(BankRepository bankRepository, BankFinanceRepository bankFinanceRepository, PreProcessService preProcessService) {
         this.bankRepository = bankRepository;
         this.bankFinanceRepository = bankFinanceRepository;
+        this.preProcessService = preProcessService;
     }
-
-    public Map<String, Object> getMinMaxAmount(String bankName) {
-        Bank bank = bankRepository.findByName(bankName)
-                .orElseThrow(EntityNotFoundException::new);
-        List<BankFinance> bankFinances = bankFinanceRepository.findAllByBankId(bank.getId());
-        Map<String, Integer> bankFinanceMap = calcYearAmountByBankId(bankFinances); // key == bankId/year
-        List<Map<String, Object>> supportAmounts = new ArrayList<>();
-
-        String minKey = Collections.min(bankFinanceMap.entrySet(),
-                Comparator.comparingInt(Map.Entry::getValue)).getKey();
-        String maxKey = Collections.max(bankFinanceMap.entrySet(),
-                Comparator.comparingInt(Map.Entry::getValue)).getKey();
-
-        supportAmounts.add(getSupportAmountMap(minKey,bankFinanceMap.get(minKey)));
-        supportAmounts.add(getSupportAmountMap(maxKey,bankFinanceMap.get(maxKey)));
+    //TODO 이름 매퍼 매핑과 관심사 분리 요망
+    public Map<String, Object> getFinanceStatus() {
+        List<BankFinance> bankFinances = bankFinanceRepository.findAll();
+        Map<String, Integer> bankFinanceMap = preProcessService.mappingAmountOfYearByBankIdAndYear(bankFinances); // key == bankId/year
+        Map<Long, String> bankNameMap = bankRepository.findAll()
+                .stream().collect(Collectors.toMap(Bank::getId, Bank::getName));
+        List<FinanceStatusByYear> financeStatusByYears = preProcessService.listingFinanceStatus(bankFinanceMap,bankNameMap);
 
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("bank", bankName);
-        resultMap.put("support_amount",supportAmounts);
+        resultMap.put("name", "주택금융 공급현황");
+        resultMap.put("status", financeStatusByYears);
         return resultMap;
-    }
-    private Map<String, Object> getSupportAmountMap(String key,Integer amount){
-        Integer year = Integer.valueOf(key.split("/")[1]);
-        Map<String, Object> supportAmountMap = new HashMap<>();
-
-        supportAmountMap.put("year", year);
-        supportAmountMap.put("amount",amount/12);
-        return supportAmountMap;
-
     }
     //TODO 메소드 구조 변경 요망
     public Map<String, Object> getHighestBank() {
         List<BankFinance> bankFinances = bankFinanceRepository.findAll();
-        Map<String, Integer> bankFinanceMap = calcYearAmountByBankId(bankFinances); // key == bankId/year
+        Map<String, Integer> bankFinanceMap = preProcessService.mappingAmountOfYearByBankIdAndYear(bankFinances); // key == bankId/year
 
         String key = Collections.max(bankFinanceMap.entrySet(),
                 Comparator.comparingInt(Map.Entry::getValue)).getKey();
@@ -71,67 +56,33 @@ public class FinanceService {
         return resultMap;
     }
 
-    //TODO 이름 매퍼 매핑과 관심사 분리 요망
-    public Map<String, Object> getFinanceStatus() {
-        List<BankFinance> bankFinances = bankFinanceRepository.findAll();
-        Map<String, Integer> bankFinanceMap = calcYearAmountByBankId(bankFinances); // key == bankId/year
+    public Map<String, Object> getMinMaxAmount(String bankName) {
+        Bank bank = bankRepository.findByName(bankName)
+                .orElseThrow(EntityNotFoundException::new);
+        List<BankFinance> bankFinances = bankFinanceRepository.findAllByBankId(bank.getId());
 
-        List<FinanceStatusByYear> financeStatusByYears = mappingFinanceStatus(bankFinanceMap);
+        Map<String, Integer> bankFinanceMap = preProcessService.mappingAmountOfYearByBankIdAndYear(bankFinances); // key == bankId/year
+        Map.Entry<String, Integer> min = Collections.min(bankFinanceMap.entrySet(),
+                Comparator.comparing(Map.Entry::getValue));
+        Map.Entry<String, Integer> max = Collections.max(bankFinanceMap.entrySet(),
+                Comparator.comparing(Map.Entry::getValue));
+
+        List<Map<String, Object>> supportAmounts = new ArrayList<>();
+        supportAmounts.add(getSupportAmountMap(min));
+        supportAmounts.add(getSupportAmountMap(max));
 
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("name", "주택금융 공급현황");
-        resultMap.put("status", financeStatusByYears);
+        resultMap.put("bank", bankName);
+        resultMap.put("support_amount", supportAmounts);
         return resultMap;
     }
 
-    private Map<String, Integer> calcYearAmountByBankId(List<BankFinance> bankFinances) {
-        Map<String, Integer> bankFinanceMap = new HashMap<>();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (BankFinance bankFinance : bankFinances) {
-            String key = stringBuilder.append(bankFinance.getBankId())
-                    .append("/")
-                    .append(bankFinance.getYear())
-                    .toString();
-            stringBuilder.setLength(0);
-            bankFinanceMap.putIfAbsent(key, 0);
-            bankFinanceMap.compute(key, (s, integer) -> integer + bankFinance.getAmount());
-        }
-        return bankFinanceMap;
-    }
+    private Map<String, Object> getSupportAmountMap(Map.Entry<String, Integer> entry) {
+        Integer year = Integer.valueOf(entry.getKey().split("/")[1]);
 
-    private List<FinanceStatusByYear> mappingFinanceStatus(Map<String, Integer> bankFinanceMap) {
-        Map<Long, String> bankMap = bankRepository.findAll()
-                .stream().collect(Collectors.toMap(Bank::getId, Bank::getName));
-
-        List<FinanceStatusByYear> financeStatusByYears = new ArrayList<>();
-        StringBuilder stringBuilder = new StringBuilder();
-
-        bankFinanceMap.forEach((key, yearAmount) -> {
-            String[] keys = key.split("/");
-            Long bankId = Long.valueOf(keys[0]);
-            String year = stringBuilder.append(keys[1])
-                    .append(" 년")
-                    .toString();
-            stringBuilder.setLength(0);
-
-            FinanceStatusByYear financeStatusByYear = FinanceStatusByYear.builder()
-                    .year(year)
-                    .totalAmount(yearAmount)
-                    .detailAmount(Collections.singletonMap(bankMap.get(bankId), yearAmount))
-                    .build();
-
-            try {
-                FinanceStatusByYear originalElement = financeStatusByYears.stream()
-                        .filter(o -> o.equals(financeStatusByYear))
-                        .findFirst().orElseThrow(NoSuchElementException::new);
-                originalElement.addTotalAmount(yearAmount);
-                originalElement.putDetailAmount(financeStatusByYear.getDetailAmount());
-            } catch (NoSuchElementException e) {
-                financeStatusByYears.add(financeStatusByYear);
-            }
-        });
-        Collections.sort(financeStatusByYears);
-
-        return financeStatusByYears;
+        Map<String, Object> supportAmountMap = new HashMap<>();
+        supportAmountMap.put("year", year);
+        supportAmountMap.put("amount", entry.getValue() / 12);
+        return supportAmountMap;
     }
 }
---
+++ src/main/java/kakaopay/housingfinance/service/PreProcessService.java
@@ -0,0 +1,61 @@
+package kakaopay.housingfinance.service;
+
+import kakaopay.housingfinance.entity.BankFinance;
+import kakaopay.housingfinance.pojo.FinanceStatusByYear;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class PreProcessService {
+
+    public Map<String, Integer> mappingAmountOfYearByBankIdAndYear(List<BankFinance> bankFinances) {
+        Map<String, Integer> bankFinanceMap = new HashMap<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (BankFinance bankFinance : bankFinances) {
+            String key = stringBuilder.append(bankFinance.getBankId())
+                    .append("/")
+                    .append(bankFinance.getYear())
+                    .toString();
+            stringBuilder.setLength(0);
+            bankFinanceMap.putIfAbsent(key, 0);
+            bankFinanceMap.compute(key, (s, integer) -> integer + bankFinance.getAmount());
+        }
+        return bankFinanceMap;
+    }
+
+
+    public List<FinanceStatusByYear> listingFinanceStatus(Map<String, Integer> bankFinanceMap, Map<Long, String> bankNameMap) {
+
+        List<FinanceStatusByYear> financeStatusByYears = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        bankFinanceMap.forEach((key, yearAmount) -> {
+            String[] keys = key.split("/");
+            Long bankId = Long.valueOf(keys[0]);
+            String year = stringBuilder.append(keys[1])
+                    .append(" 년")
+                    .toString();
+            stringBuilder.setLength(0);
+
+            FinanceStatusByYear financeStatusByYear = FinanceStatusByYear.builder()
+                    .year(year)
+                    .totalAmount(yearAmount)
+                    .detailAmount(Collections.singletonMap(bankNameMap.get(bankId), yearAmount))
+                    .build();
+
+            try {
+                FinanceStatusByYear originalElement = financeStatusByYears.stream()
+                        .filter(o -> o.equals(financeStatusByYear))
+                        .findFirst().orElseThrow(NoSuchElementException::new);
+                originalElement.addTotalAmount(yearAmount);
+                originalElement.putDetailAmount(financeStatusByYear.getDetailAmount());
+            } catch (NoSuchElementException e) {
+                financeStatusByYears.add(financeStatusByYear);
+            }
+        });
+        Collections.sort(financeStatusByYears);
+
+        return financeStatusByYears;
+    }
+}
